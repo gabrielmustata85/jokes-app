1d1a5cca1f4b9e8415eb9e00b82bd705e9c771e3
gabrielmustata85 <gabrielmstt@gmail.com>
2021-06-13 14:48:20 +0300

    controller added

---
+++ src/main/java/controllers/JokesController.java
@@ -0,0 +1,26 @@
+package controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import services.JokesService;
+
+@Controller
+public class JokesController {
+
+    private final JokesService jokesService;
+
+    public JokesController(JokesService jokesService) {
+        this.jokesService = jokesService;
+    }
+
+    @RequestMapping({"/",""})
+    public String showJokes(Model model){
+
+        model.addAttribute("jokes", jokesService.getJoke());
+
+        return "index";
+
+    }
+}
